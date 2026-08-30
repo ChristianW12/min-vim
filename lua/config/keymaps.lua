143877f30c8b6f225919c83dbab82ee70525a4c1
ChristianW12 <christian.wagner1204@gmail.com>
2026-08-30 15:39:59 +0200

    Add insert mode LSP completion keymaps
    Map <C-Space> and terminal fallback <C-@> to omni-completion in insert
    mode and reformat Neo-tree keymap definitions.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -57,25 +57,23 @@ map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "move selected lines down" })
 map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "move selected lines up" })
 
 -- =========================
+-- Completion (LSP / Omni-Completion)
+-- =========================
+-- Ctrl + Space triggert die Code-Vervollständigung im Insert-Modus
+map("i", "<C-Space>", "<C-x><C-o>", { desc = "Trigger LSP Completion", silent = true })
+map("i", "<C-@>", "<C-x><C-o>", { desc = "Trigger LSP Completion (Terminal Fallback)", silent = true })
+
+-- =========================
 -- Explorer 
 -- =========================
 -- map("n", "<leader>e", ":Lexplore<CR>", { desc = "Toggle Explorer", silent = true })
 -- map("n", "<leader>fe", ":Lexplore<CR>", { desc = "Focus Explorer", silent = true })
 -- map("n", "<leader>E", ":Lexplore<CR>", { desc = "Toggle file explorer", silent = true })
-map("n", "<leader>e", "<cmd>Neotree toggle filesystem reveal left<CR>", {
-    desc = "Toggle Explorer",
-    silent = true,
-})
+map("n", "<leader>e", "<cmd>Neotree toggle filesystem reveal left<CR>", { desc = "Toggle Explorer", silent = true, })
 
-map("n", "<leader>fe", "<cmd>Neotree focus filesystem left<CR>", {
-    desc = "Focus Explorer",
-    silent = true,
-})
+map("n", "<leader>fe", "<cmd>Neotree focus filesystem left<CR>", { desc = "Focus Explorer", silent = true, })
 
-map("n", "<leader>E", "<cmd>Neotree filesystem reveal float<CR>", {
-    desc = "Floating Explorer",
-    silent = true,
-})
+map("n", "<leader>E", "<cmd>Neotree filesystem reveal float<CR>", { desc = "Floating Explorer", silent = true, })
 
 -- =========================
 -- Telescope Keymaps (ehemals in lazy keys={})
